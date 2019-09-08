cc4354ee1d336783a64725c3902b8b5d1666d4bf
tacongnam <49036956+tacongnam@users.noreply.github.com>
2019-09-08 16:42:05 +0700

    Create PR003004.cpp

---
+++ Wiki/PR003004.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/// Digit DP template
+
+long long dp[20][200][3];
+vector <long long> digit;
+
+void getdigit(long long x){
+    digit.clear();
+    while(x != 0){
+        digit.push_back(x % 10);
+        x = x / 10;
+    }
+}
+
+long long getsum(int id, long long sum, int tight){
+    if(id == -1) //Nếu đã xét hết các chữ số
+        return sum;
+    if(dp[id][sum][tight] != -1 && tight == 0)
+        return dp[id][sum][tight];
+    long long res = 0;
+    int k = (tight == 0) ? 9 : digit[id];
+    for(int i = 0; i <= k; i++){
+        int newt = (i == digit[id]) ? tight : 0;
+        res += getsum(id - 1, sum + i, newt);
+    }
+    if(tight == 0)
+        dp[id][sum][tight] = res;
+    return res;
+}
+
+long long query(long long l, long long r){
+    memset(dp, -1, sizeof(dp));
+    getdigit(l - 1);
+    long long a = getsum(digit.size() - 1, 0, 1);
+    getdigit(r);
+    long long b = getsum(digit.size() - 1, 0, 1);
+    return b - a;
+}
+
+/// End of template
+long long l, r, n;
+
+main(){
+    scanf("%lld", &n);
+    for(int i = 1; i <= n; i++){
+        scanf("%lld%lld", &l, &r);
+        printf("%lld\n", query(l, r));
+    }
+}
