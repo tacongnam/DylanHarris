b99566053e23573ccc8c5be46f3c8ca7d12b6c80
tacongnam <49036956+tacongnam@users.noreply.github.com>
2019-10-18 16:51:09 +0700

    Sol of DBG

--- VOI2020/18_10_2019/DBG.cpp
+++ VOI2020/18_10_2019/DBG.cpp
@@ -1,98 +1,127 @@
+				///************** H.A.N Smile **************///
 #include <bits/stdc++.h>
-#define maxn 1005
+#define f first
+#define s second
+#define mp make_pair
+#define MOD 1000000007
+#define HAN "dbg"
 using namespace std;
-
-int f[maxn], w[maxn], n, a, b, d, ans = INT_MAX;
-
-void sub1() { //Generate all permutations O(n!.n)
-    vector <int> dd, vt;
-    int t = 0;
-    for(int i = 1; i <= n; i++)
-        t += f[i];
-    for(int t = max(a, d - t - 1); t <= min(b, d + t + 1); t++) {
-        vt.clear();
-        for(int i = 1; i <= n; i++)
-            vt.push_back(i);
-        do {
-            int res = 0, timenow = t;
-            for(int i = 0; i < n; i++) {
-                timenow += f[vt[i]];
-                res += abs(timenow - d) * w[vt[i]];
-                if(res >= ans || timenow > b){
-                    res = INT_MAX;
-                    break;
-                }
-            }
-            ans = min(ans, res);
-        } while(next_permutation(vt.begin(), vt.end()));
-    }
-    cout << ans;
-}
-pair<int, int> pi[maxn];
-bool isgreater(int x1, int x2, int y1, int y2){
-    return (x1 * y2) > (x2 * y1);
-}
-
-bool cmp (pair<int, int> a, pair<int, int> b){
-    return isgreater(a.first, a.second, b.first, b.second);
-}
-
-void sub3(){
+const int N = 1005;
+int n , a , b ,c , d, dem;
+pair <int , int> h[N];
+bool dd[N];
+long long ans, maxx = 1e18, p;
+void sub12()
+{
+    sort( h+1 , h+1+n);
+    for (int i = 0 ; i <= b - a - p; i++){
+    do {
+    dem = a + i;
     ans = 0;
-    int timenow = a;
-    for(int i = 1; i <= n; i++){
-        timenow += pi[i].second;
-        ans += abs(timenow - a) * pi[i].first;
+    for (int i = 1; i <= n; i++){
+        dem += h[i].f;
+        ans += 1ll* abs(d - dem) * h[i].s;
+        if (ans > maxx) break;
     }
-    cout << ans;
+    maxx = min(maxx, ans);
+    }while (next_permutation(h+1,h+1+n));
+    }
+    cout <<maxx;
+
+}
+bool cmp( pair <int , int > a , pair <int , int> b)
+{
+    return (1ll*a.f*b.s < 1ll*b.f*a.s);
+}
+bool cmpp( pair <int , int > a , pair <int , int> b)
+{
+    return (1ll*a.s*b.f > 1ll*b.s*a.f);
+}
+void sub3()
+{
+    sort( h+1 , h+1+n, cmp);
+    dem = a;
+    for (int i = 1 ; i <= n; i++)
+    {
+        dem += h[i].f;
+        ans += 1ll*abs(dem - d)*h[i].s;
+    }
+    cout <<ans;
 }
 
-void sub4(){
-    ans = 0;
-    int timenow = b;
-    for(int i = 1; i <= n; i++){
-        ans += abs(timenow - b) * pi[i].first;
-        timenow -= pi[i].second;
-    }
-    cout << ans;
+void sub4()
+{
+    sort(h+1 , h+1+n , cmp);
+        dem = b;
+        for (int i = 1; i <= n; i++)
+        {
+            ans += 1ll*abs(dem-d)*h[i].s;
+            dem -= h[i].f;
+        }
+    cout <<ans;
 }
 
-void dq(int id, int kq, int boundl, int boundr){
-    if(id == n){
-        ans = min(ans, kq);
+void duyet(int i , int st, int fn , long long sum)
+{
+    if (i > n){
+        maxx = min(maxx , sum);
         return;
     }
-    if(boundl - pi[id + 1].second >= a && kq + abs(boundl - d) * pi[id + 1].first < ans)
-        dq(id + 1, kq + abs(boundl - d) * pi[id + 1].first, boundl - pi[id + 1].second, boundr);
-    if(boundr + pi[id + 1].second <= b && kq + abs(boundr + pi[id + 1].second - d) * pi[id + 1].first < ans)
-        dq(id + 1, kq + abs(boundr + pi[id + 1].second - d) * pi[id + 1].first, boundl, boundr + pi[id + 1].second);
+
+    if ( fn + h[i].f <= b) duyet(i+1 , st, fn + h[i].f , sum + 1ll*abs(fn + h[i].f - d)* h[i].s);
+    if ( st - h[i].f >= a) duyet(i+1 , st - h[i].f , fn , sum+ 1ll*abs(st - d) * h[i].s);
 }
 
-void sub256(){
-    for(int i = max(a, d - 100); i <= min(b, d + 100); i++)
-        dq(0, 0, i, i);
-    cout << ans;
+void sub56()
+{
+    sort(h+1, h+1+n , cmpp);
+    for (int i = d-100 ; i <= d+100; i++)
+        if (i >= a && i + h[1].f <= b) duyet(2 , i , i + h[1].f , 1ll*abs( i + h[1].f - d)*h[1].s );
+    cout <<maxx;
 }
 
-main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0);
-    freopen("DBG.inp", "r", stdin);
-    freopen("DBG.out", "w", stdout);
-    cin >> n >> a >> b >> d;
-    for(int i = 1; i <= n; i++)
-        cin >> f[i] >> w[i];
-    if(n <= 3){
-        sub1();
-        return 0;
+void sub7()
+{
+    sort(h+1, h + 1 + n , cmp);
+    for (int i = d - 10000 ; i <= d + 10000; ++i) if (i >= a && i + h[1].f <= b)
+    {
+        long long res = 0;
+        int st = i;
+        int fn = i + h[1].f;
+        bool ch = 1;
+        res += 1ll*abs(d-fn)*h[1].s;
+        for (int j = 2 ; j <= n; ++j){
+            if (st - h[j].f >= a && (1ll*abs(d-fn-h[j].f)*h[j].s > 1ll*abs(d-st)*h[j].s || fn + h[j].f > b)){
+                res += 1ll*abs(d-st)*h[j].s;
+                st -= h[j].f;
+            }
+            else if (fn + h[j].f <= b) {
+                fn += h[j].f;
+                res += 1ll*abs(d-fn)*h[j].s;
+            }
+            else {
+                ch = 0;
+                break;
+            }
+        }
+        if (ch) maxx = min(maxx, res);
     }
-    for(int i = 1; i <= n; i++)
-        pi[i] = make_pair(w[i], f[i]);
-    sort(pi + 1, pi + n + 1, cmp);
-    if(a == d)
-        sub3();
-    else if(b == d)
-        sub4();
-    else
-        sub256();
+    cout <<maxx;
+}
+
+main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	freopen(HAN".inp" , "r", stdin);
+	freopen(HAN".out" , "w", stdout);
+	cin >> n >> a >> b >> d;
+    for (int i = 1; i <= n ; i++) cin >> h[i].f >> h[i].s, p+= h[i].f;
+    /*if (n <= 8) sub12();
+    else if (a==d) sub3();
+    else if (b==d) sub4();
+    else if (n <= 20) sub56();
+    else*/
+        sub7();
 }
